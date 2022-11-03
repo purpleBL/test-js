870f7d1d2688024fe1f1707ff014a82a09f839ae
purpleBL <blindspurple@gmail.com>
2022-11-04 03:03:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-
-
-document.addEventListener("touchmove", function (event) {
-  if (event.scale !== 1) event.preventDefault();
-});
-
-
-
 const menuBtn = document.querySelector(".menu-btn");
+
 menuBtn.onclick = () => {
   menuBtn.classList.toggle("menu-btn__active");
 };
